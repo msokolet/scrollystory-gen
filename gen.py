7bdde38fec66cad33c9775a0e61c741dc29b0d06
Michael Sokoletsky <michael.sokoletsky@weizmann.ac.il>
2022-02-24 10:12:03 +0200

    create html dir

--- gen.py
+++ gen.py
@@ -14,8 +14,12 @@ templates_dir = os.path.join(root, 'templates')
 env = Environment( loader = FileSystemLoader(templates_dir) )
 template = env.get_template(f'{opts["template"]}.html')
 
+# generate html folder if it doesn't exist
+html_dir = os.path.join(root, 'html')
+if not os.path.exists(html_dir): os.makedirs(html_dir)
+
 # generate html page
-filename = os.path.join(root, 'html', f'{opts["page_name"]}.html')
+filename = os.path.join(html_dir, f'{opts["page_name"]}.html')
 with open(filename, 'w') as fh:
     fh.write(template.render(
         page_title = opts['page_title'],
